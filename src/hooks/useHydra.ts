43247551481ade9c2228845273d290ab724cd977
Ivaylo Petrov <isivaylopetrov@gmail.com>
2022-08-22 09:15:04 +0300

    add useHydra hook

---
+++ src/hooks/useHydra.ts
@@ -0,0 +1,81 @@
+import { Hydraweb3 } from 'hydraweb3-js'
+import { useState, useEffect } from 'react'
+import invoke from '../hydra/mobileInvoke/browser'
+
+const rawCall = invoke.bind('rawCall')
+
+declare global {
+  interface Window {
+    hydrawallet: any
+    ReactNativeWebView: any
+  }
+}
+
+export default function (): { walletExtension: any; hydraweb3Extension: any; error: string } {
+  const [walletExtension, setWalletExtension] = useState({})
+  const [hydraweb3Extension, setHydraweb3Extension] = useState({})
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    if (window.hydrawallet) {
+      return
+    }
+    window.addEventListener(
+      'message',
+      function handleHydrawalletInstalledOrUpdated(event) {
+        if (event.data.message && event.data.message.type === 'HYDRAWALLET_INSTALLED_OR_UPDATED') {
+          // Refresh the page
+          window.location.reload()
+        }
+      },
+      false
+    )
+
+    window.addEventListener(
+      'message',
+      function handleHydrawalletAcctChanged(event) {
+        if (event.data.message && event.data.message.type === 'HYDRAWALLET_ACCOUNT_CHANGED') {
+          if (event.data.message.payload.error) {
+            // handle error
+            setError(event.data.message.payload.error)
+          }
+          setError('')
+          // should be in state where wallet or contract functions are used
+          const account = event.data.message.payload.account
+          const hydraExtension = new Hydraweb3(window.ReactNativeWebView ? { rawCall } : window.hydrawallet.rpcProvider)
+          setWalletExtension(account)
+          setHydraweb3Extension(hydraExtension)
+        }
+      },
+      false
+    )
+
+    // Handle incoming messages
+    window.addEventListener(
+      'message',
+      function handleMessage(message) {
+        if (message.data.target === 'hydrawallet-inpage') {
+          // result: object
+          // error: string
+          const { result, error } = message.data.message.payload
+          if (error) {
+            if (error === 'Not logged in. Please log in to hydrawallet first.') {
+              // Show an alert dialog that the user needs to login first
+            } else {
+              // Handle different error than not logged in...
+            }
+            return
+          }
+
+          if (message?.data?.payload?.statusChangeReason === 'Account Logged Out') {
+            window.postMessage({ message: { type: 'CONNECT_HYDRAWALLET' } }, '')
+          }
+          // Do something with the message result...
+        }
+      },
+      false
+    )
+  }, [])
+
+  return { walletExtension, hydraweb3Extension, error }
+}
---
+++ src/hydra/mobileInvoke/messager/index.js
@@ -0,0 +1,148 @@
+import { createEventBus } from './event-bus'
+
+const SYNC_COMMAND = 'RNWV:sync'
+const STATUS_SUCCESS = 'success'
+const STATUS_FAIL = 'fail'
+let _count = 0
+
+class Deferred {
+  constructor() {
+    this.promise = new Promise((resolve, reject) => {
+      this.resolve = resolve
+      this.reject = reject
+    })
+  }
+}
+
+function getTransactionKey(data) {
+  return `${data.command}(${data.id})`
+}
+
+function createPayload(command, data) {
+  return {
+    id: _count++,
+    command,
+    data,
+    reply: false,
+    status: STATUS_SUCCESS
+  }
+}
+
+export function createMessager(sendHandler) {
+  let needWait = []
+  const eventBus = createEventBus()
+  const transactions = {}
+  const callbacks = {} //
+  const fn = {} // all other side functions
+
+  function isConnect() {
+    return !needWait
+  }
+
+  function bind(name) {
+    return (...args) => send(name, args)
+  }
+
+  function define(name, func) {
+    callbacks[name] = args => func(...args)
+    !needWait && sync()
+    return { define, bind }
+  }
+
+  /** sender parts */
+  function sender(data) {
+    const force = data.command === SYNC_COMMAND // force send the message when the message is the sync message
+    if (!force && needWait) {
+      needWait.push(data)
+    } else {
+      sendHandler(data)
+    }
+    eventBus.emitEvent('send', data)
+  }
+  function initialize() {
+    if (needWait) {
+      const waiting = needWait
+      needWait = null
+      waiting.forEach(function(payload) {
+        sender(payload)
+      })
+      eventBus.emitEvent('ready')
+    }
+  }
+
+  function send(command, data) {
+    const payload = createPayload(command, data)
+    const defer = new Deferred()
+    transactions[getTransactionKey(payload)] = defer
+    sender(payload)
+    return defer.promise
+  }
+
+  function reply(data, result, status) {
+    data.reply = true
+    data.data = result
+    data.status = status
+    sender(data)
+  }
+
+  /** listener parts */
+  function listener(data) {
+    if (data.reply) {
+      const key = getTransactionKey(data)
+      if (transactions[key]) {
+        if (data.status === STATUS_FAIL) {
+          transactions[key].reject(data.data)
+        } else {
+          transactions[key].resolve(data.data)
+        }
+      }
+    } else {
+      if (callbacks[data.command]) {
+        const result = callbacks[data.command](data.data)
+        if (result && result.then) {
+          result
+            .then(function(d) {
+              reply(data, d, STATUS_SUCCESS)
+            })
+            .catch(function(e) {
+              reply(data, e, STATUS_FAIL)
+            })
+        } else {
+          reply(data, result, STATUS_SUCCESS)
+        }
+      } else {
+        reply(data, `function ${data.command} is not defined`, STATUS_FAIL)
+      }
+    }
+    eventBus.emitEvent('receive', data)
+  }
+
+  const __sync = bind(SYNC_COMMAND)
+  function _sync(defines = []) {
+    defines
+      .filter(function(d) {
+        return !fn[d]
+      })
+      .map(function(d) {
+        fn[d] = bind(d)
+      })
+    initialize()
+    return Object.keys(callbacks)
+  }
+  define(SYNC_COMMAND, _sync)
+
+  function sync() {
+    __sync(Object.keys(callbacks)).then(_sync)
+  }
+
+  return {
+    bind,
+    define,
+    listener,
+    ready: sync,
+    fn,
+    addEventListener: eventBus.addEventListener,
+    removeEventListener: eventBus.removeEventListener,
+    isConnect
+  }
+}
