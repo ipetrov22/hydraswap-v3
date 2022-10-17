53c622673315bcb0bcb8c25549273c77ff0ea166
Ivaylo Petrov <isivaylopetrov@gmail.com>
2022-10-17 15:00:53 +0300

    change uniswap mention to hydraswap

--- src/pages/MigrateV2/MigrateV2Pair.tsx
+++ src/pages/MigrateV2/MigrateV2Pair.tsx
@@ -385,7 +385,7 @@ function V2PairMigration({
         {chainId && migrator && (
           <ExternalLink href={getExplorerLink(chainId, migrator.address, ExplorerDataType.ADDRESS)}>
             <ThemedText.DeprecatedBlue display="inline">
-              <Trans>Uniswap migration contract↗</Trans>
+              <Trans>Hydraswap migration contract↗</Trans>
             </ThemedText.DeprecatedBlue>
           </ExternalLink>
         )}
