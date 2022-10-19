f3f6c692888d0a697933ca58bec3cfa6c9cab6c5
Ivaylo Petrov <isivaylopetrov@gmail.com>
2022-10-19 12:23:28 +0300

    Currency class to support testneet

--- src/utils/unwrappedToken.ts
+++ src/utils/unwrappedToken.ts
@@ -1,8 +1,8 @@
 import { Currency } from '@uniswap/sdk-core'
-import { HYDRA, WHYDRA } from 'hydra/sdk'
+import { HYDRA_CURRENCIES, WHYDRA } from 'hydra/sdk'
 
 export function unwrappedToken(currency: Currency): Currency {
   const chainId: keyof typeof WHYDRA = currency.chainId
-  if (currency.equals(WHYDRA[chainId])) return HYDRA
+  if (currency.equals(WHYDRA[chainId])) return HYDRA_CURRENCIES[chainId]
   return currency
 }
